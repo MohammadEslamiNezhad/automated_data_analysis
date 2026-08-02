e8867b6ed25a101e76353181ed93aa9a7a250ae4
MohammadEslamiNezhad <mhmdaslamynzhad7@gmail.com>
2026-08-02 08:11:47 +0330

    complete data profile adding datetime and correlation need to refactor

--- app/preproccess.py
+++ app/preproccess.py
@@ -1,8 +1,22 @@
 import pandas as pd 
 from pandas.api.types import is_numeric_dtype, is_datetime64_any_dtype
 
-dataset_path = "./dataset/diabetes.csv"
-dataset = pd.read_csv(dataset_path)
+# TODO : code must be a class or a function to input data from API 
+# ----------------/for test/---------------- 
+dataset_path = "./dataset/diabetes.csv" 
+dataset_path = "./dataset/Start-Data-Analysis.xlsx"
+
+if dataset_path.endswith('.xlsx'):
+    dataset = pd.read_excel(dataset_path, sheet_name='Raw', header=1)
+elif dataset_path.endswith('.csv'):
+    dataset = pd.read_csv(dataset_path)
+# ----------------/for test/---------------- 
+
+unnamed_cols = dataset.columns[dataset.columns.str.contains('Unnamed:')].to_list()
+empty_cols = lambda col: dataset[col].isnull().all()
+remove_cols = [col for col in unnamed_cols if empty_cols(col)]
+dataset = dataset.drop(columns=remove_cols)
+
 duplicated = dataset.duplicated()
 
 def find_uniques(col: pd.Series):
@@ -26,7 +40,8 @@ def find_uniques(col: pd.Series):
 def is_numeric(col: pd.Series):
     return is_numeric_dtype(col)
 
-numeric_filter = dataset.apply(is_numeric).index
+numeric_filter = dataset.apply(is_numeric)
+numeric_filter = numeric_filter[numeric_filter].index
 numeric_dataset = dataset[numeric_filter]
 
 cardinality_thresh = 90
@@ -45,7 +60,7 @@ numeric_columns_summary = {
     'IQR': IQR,
     'statistic MIN': describe.loc['25%'] - 1.5 * IQR,
     'statistic MAX': describe.loc['75%'] + 1.5 * IQR,
-    'Skewness': dataset.skew(),
+    'Skewness': numeric_dataset.skew(),
 }
 outlier_filter = (numeric_dataset > numeric_columns_summary['statistic MAX']) & (numeric_dataset < numeric_columns_summary['statistic MIN'])
 numeric_columns_summary['Statistical Outliers (IQR)'] = numeric_dataset[outlier_filter]
@@ -65,7 +80,9 @@ def find_top_value(col: pd.Series, just_top=True, dataset: pd.DataFrame | pd.Ser
     elif type(dataset) == pd.DataFrame:
         col_name = col.name
         all_freq = dataset.groupby(col_name)[col_name].count().sort_values(ascending=False)
-        return all_freq if not just_top else all_freq.iloc[0]
+        if not all_freq.empty:
+            return all_freq if not just_top else all_freq.iloc[0]
+            
     else: 
         all_freq = dataset.groupby(dataset).count().sort_values(ascending=False)
         return all_freq if not just_top else all_freq.iloc[0]
@@ -77,17 +94,34 @@ categorical_columns_summary = {
     'Frequency': categorical_prob_dataset.apply(find_top_value, args=(False, categorical_prob_dataset))
 }
 
-'''
-Datetime Columns TODO 
-│
-├── Min Date
-├── Max Date
-└── Missing
+def find_date_dtype(col):
+    return is_datetime64_any_dtype(col)
 
-Correlation Matrix TODO
-integration TODO
-'''
+date_filter = dataset.apply(find_date_dtype)
+date_filter = date_filter[date_filter].index
+date_dataset = dataset[date_filter]
+
+date_column_summary = {
+    'Min Date': date_dataset.min(),
+    'Max Date': date_dataset.max(),
+    'Missings': date_dataset[date_dataset.isnull()].count()
+}
+
+correlation = numeric_dataset.corr()
+
+all_data_profile = {
+    'Dataset Summary': dataset_summary,
+    'Quality Report': quality_report,
+    'Numeric Columns Summary': numeric_columns_summary,
+    'Categorical Columns Summary': categorical_columns_summary,
+    'Date Column Summary': date_column_summary,
+    'Correlation': correlation
+}
+
 print(dataset_summary)
 print(quality_report)
 print(numeric_columns_summary)
 print(categorical_columns_summary)
+print(date_column_summary)
+print(correlation)
+print(all_data_profile)
