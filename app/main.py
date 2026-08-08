7fa560aed335854056e51d1ea1f0f0ff69cc2023
MohammadEslamiNezhad <mhmdaslamynzhad7@gmail.com>
2026-08-08 17:42:15 +0330

    integrating preprocessing module to project

--- app/main.py
+++ app/main.py
@@ -1,5 +1,16 @@
-@app.post('/upload_file/')
+from preproccess import * 
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+@app.get('/')
+def root():
+    return {'message': 'OKAY!'}
+
+@app.post('/uploadfile/')
 async def upload_file(file: UploadFile = File(...)):
+    print('start method')
+
     global data 
     if file.filename.endswith('.csv'):
         data = pd.read_csv(file.file)
@@ -8,4 +19,8 @@ async def upload_file(file: UploadFile = File(...)):
     else: 
         return {'message': 'choose right file type'}
 
+    profiler = DatasetProfiler(data)
+    profile = profiler.build_profile()
+    print(json.dumps(profile, indent=2, ensure_ascii=False))
+    
     return {'detail': 'file uploaded successfully'}
